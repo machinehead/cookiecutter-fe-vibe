67edf2d6f476228da7061c3654acbb9202ae98d9
machinehead <nekhayiv@gmail.com>
2025-05-18 14:01:50 -0500

    add standard configurations

---
+++ {{cookiecutter.project_slug}}/.github/copilot-instructions.md
@@ -0,0 +1,3 @@
+In Next.js 15 params passed into Page and Layout components and searchParams passed into Page components are now 
+Promises: https://nextjs.org/docs/messages/next-prerender-sync-params .
+
---
+++ {{cookiecutter.project_slug}}/eslint.config.mjs
@@ -0,0 +1,23 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+import pluginNext from "@next/eslint-plugin-next";
+import { defineConfig, globalIgnores } from "eslint/config";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    languageOptions: { globals: { ...globals.browser, ...globals.node } },
+  },
+  tseslint.configs.recommended,
+  pluginReact.configs.flat["jsx-runtime"],
+  pluginNext.flatConfig.recommended,
+  pluginNext.flatConfig.coreWebVitals,
+  globalIgnores([".next/*", "**/generated/**"]),
+]);
